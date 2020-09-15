f88e4df824284d066fcba814f4a15cb28084b05f
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-15 00:51:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,12 +49,13 @@ def monopoly_with_fourth_tier()
 	monopoly = {
     :railroads => {
       :pieces => 4
-      } rent_in_dollars => {
+      :rent_in_dollars => [
         :one_piece_owned =>25,
         :two_pieces_owned =>50,
         :three_pieces_owned =>100,
         :four_pieces_owned =>200
-      } names => {
+      ], 
+      :names => [
         :reading_railroad => {
         "mortgage_value"=>"$100"
         },
@@ -67,6 +68,8 @@ def monopoly_with_fourth_tier()
       :shortline => {
         "mortgage_value"=>"$800"
         }
-      }
+      ]
+	  }
+	}
 end
 
